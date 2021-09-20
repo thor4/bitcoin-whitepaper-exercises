f10ee21ed25e8974d11b89af0877dfc66f900b16
Bryan Conklin <6412103+thor4@users.noreply.github.com>
2021-09-20 17:50:08 -0400

    finish part 1 of hashing

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var crypto = require("crypto");
+var crypto = require("crypto"); // load the crypto library that comes with node
 
 // The Power of a Smile
 // by Tupac Shakur
@@ -28,8 +28,23 @@ Blockchain.blocks.push({
 });
 
 // TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+console.log(Blockchain.blocks[0])
+for (let line of poem) {
+	createBlock(line)
+}
+
+function createBlock(_data) { // use underscore to indicate a mutable variable and differentiate it from the local instance
+	let block = {
+		index: Blockchain.blocks.length, // since we start with index=0, this gives the current position
+		prevHash: Blockchain.blocks[Blockchain.blocks.length-1].hash, // extract previous block's hash
+		data: _data,
+		timestamp: Date.now()
+	}
+	block.hash = blockHash(block)
+	Blockchain.blocks.push(block)
+	console.log(block)
+	return block
+}
 
 // console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
 
@@ -37,7 +52,10 @@ Blockchain.blocks.push({
 // **********************************
 
 function blockHash(bl) {
+	// block = JSON.stringify(bl) // alternate method for passing object attributes as strings
 	return crypto.createHash("sha256").update(
 		// TODO: use block data to calculate hash
+		`${bl.index};${bl.prevHash};${bl.data};${bl.timestamp};`
+		// block // alternate method
 	).digest("hex");
 }
